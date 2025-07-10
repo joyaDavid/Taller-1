a0f5deee99762e4420d424e48a80b07fe0b6690d
joyaDavid <juandavidjoyariano@gmail.com>
2025-07-10 16:39:10 -0500

    Add files via upload

---
+++ projectN1/src/main/java/com/mycompany/projectn1/ProjectN1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projectn1;
+
+/**
+ *
+ * @author PC HP
+ */
+public class ProjectN1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
